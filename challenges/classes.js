49035c026906a9232fae1c13de0a06a9681cfea9
bkoehler2016 <bkoehler85282@gmail.com>
2019-10-11 19:26:26 -0700

    added formula for cube for volume and surface area

--- challenges/classes.js
+++ challenges/classes.js
@@ -29,3 +29,25 @@ console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker1 {
+    constructor(attribute) {
+        super(attribute);
+    }
+    cubeVolume() {
+        return this.length * this.height * this.width;
+    }
+    cubeSurfaceArea() {
+        let numberOfSides = 6;
+        return numberOfSides * (this.length * this.length);
+    }
+}
+
+const cube = new CubeMaker({
+    length: 7,
+    width: 7,
+    height: 7
+});
+
+console.log(cube.cubeVolume());
+console.log(cube.cubeSurfaceArea());
